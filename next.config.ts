4a9adc9aae982325755d0ae0dfedd697a85a22c2
Mário Barbosa <mariogomesbarbosa@gmail.com>
2026-09-25 15:48:28 -0300

    ci: configure GitHub Pages automated deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? "/rogue-term" : "",
+  assetPrefix: isProd ? "/rogue-term" : undefined,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
